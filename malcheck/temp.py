81ca4f0832c47bf1615c1b379bd0f14d2825f523
saip007 <saip4622@gmail.com>
2022-12-26 18:24:37 +0530

    adding some temporary files for testing

---
+++ malcheck/temp.py
@@ -0,0 +1,16 @@
+from os import system,getcwd
+
+
+# def _ffiles(_path):
+#     for i,j,k in walk(_path):
+#         print(i,j,k)
+
+
+def main():
+    dir_path = r'/home/ssambarathi/Documents/mal-pac//test/'
+
+try:
+    if __name__ == "__main__":
+        main()
+except Exception as ee:
+    print("exception is : {}".format(ee)) 
