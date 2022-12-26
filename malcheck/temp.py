b10358572c144168ff2bd3b7da72f8a02cef1336
saip007 <saip4622@gmail.com>
2022-12-26 18:59:19 +0530

    added some code

--- malcheck/temp.py
+++ malcheck/temp.py
@@ -1,16 +1,30 @@
-from os import system,getcwd
+from os import walk
+
+_res = {}
 
 
-# def _ffiles(_path):
-#     for i,j,k in walk(_path):
-#         print(i,j,k)
+
+def get_files(_path):
+    files_list,files = [],{}
+    for i,j,k in walk(_path):
+        if files.get(i) != None:
+            files[i] = files[i]+k
+        else:
+            files[i] = k
+    for _j in files:
+        if len(files[_j]) != 0:
+            for _i in files[_j]:
+                files_list.append(_j+'/'+_i)
+    return files_list
 
 
 def main():
-    dir_path = r'/home/ssambarathi/Documents/mal-pac//test/'
+    directory_path = r'/home/ssambarathi/Documents/mal-pac/malcheck/test'
+    list_of_files = (get_files(directory_path))
+
 
 try:
     if __name__ == "__main__":
         main()
 except Exception as ee:
-    print("exception is : {}".format(ee)) 
+    print("exception is : {}".format(ee))
