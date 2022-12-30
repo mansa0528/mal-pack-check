121c48b130d1c900fb126068178bf160e0b89806
saip007 <saip4622@gmail.com>
2022-12-30 10:10:44 +0530

    changes added

--- malcheck/temp.py
+++ malcheck/temp.py
@@ -1,7 +1,19 @@
 from os import walk
+import re
 
-_res = {}
+_res = {'urls':[]}
 
+def rEgx(files_to_open):
+    _reg = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)'
+    for _file in files_to_open:
+        try:
+            _f = open(_file,'r').read()
+            x = re.findall(_reg,_f)
+            _res['urls'] = _res.get('urls')+x
+            break
+        except Exception as e:
+            print(e)
+    return list(_res.values())
 
 
 def get_files(_path):
@@ -21,7 +33,7 @@ def get_files(_path):
 def main():
     directory_path = r'/home/ssambarathi/Documents/mal-pac/malcheck/test'
     list_of_files = (get_files(directory_path))
-
+    print(rEgx(list_of_files))
 
 try:
     if __name__ == "__main__":
